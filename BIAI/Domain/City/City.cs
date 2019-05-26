41fc0ec06224efb60ac919354f0778ac692ed561
Dominik Mura <dominikmura8@gmail.com>
2019-05-26 22:56:37 +0200

    initial commit

---
+++ BIAI/Domain/City/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.City
+{
+    class City
+    {
+        public string CityName { set; get; }
+        public double ParameterX { set; get; }
+        public double ParameterY { set; get; }
+
+    }
+}
---
+++ BIAI/Presentation/Controllers/SalesmanController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ViewModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Presentation.Controllers
+{
+   
+    public class SalesmanController : Controller
+    {
+        public SalesmanController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult MainPage()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult MainPage(FileViewModel file)
+        {
+            var name = file.File.OpenReadStream();
+            StreamReader stream = new StreamReader(name);
+            
+            return View();
+        }
+    }
+}
