62d234a64ee58e9a94d7362bef775eb24e7210b4
Dominik Mura <dominikmura8@gmail.com>
2019-05-27 23:25:17 +0200

    final oplementation of Path

---
+++ BIAI/Domain/Environment/Environment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Environment
+{
+    static class Environment
+    {
+        public static double MutateRate { set; get; }
+        public static int PopulationSize { set; get; }
+        public static int CitiesNumber { set; get; }
+        public static int Elitism { set; get; }
+    }
+}
--- BIAI/Domain/Path/Path.cs
+++ BIAI/Domain/Path/Path.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Domain.Path
@@ -9,24 +10,27 @@ namespace Domain.Path
         public IList<City.City> Cities { private set; get; }
         public double Distance { private set; get; }
         public double Fitness { private set; get; }
+        private Random Random { set;  get; }
 
         public Path(List<Domain.City.City>cities)
         {
             Cities = cities;
             Distance = CalculateDistance();
             Fitness = CalculateFitness();
+            Random=new Random();
+
         }
 
         public Path Shuffle()
         {
             List<City.City> list = new List<City.City>(Cities);
-            Random random=new Random();
+            
             int n = list.Count;
 
             while (n > 1)
             {
                 --n;
-                int k = random.Next(n + 1);
+                int k = Random.Next(n + 1);
                 City.City v = list[k];
                 list[k] = list[n];
                 list[n] = v;
@@ -34,6 +38,35 @@ namespace Domain.Path
             return new Path(list);
         }
 
+        public Path Crossover(Path path)
+        {
+            
+            int i = Random.Next(0, path.Cities.Count);
+            int j = Random.Next(i, path.Cities.Count);
+            List<City.City> rangedList = Cities.ToList().GetRange(i, j - i + 1);
+            List<City.City> ms = path.Cities.Except(rangedList).ToList();
+            List<City.City> c = ms.Take(i)
+                .Concat(rangedList)
+                .Concat(ms.Skip(i))
+                .ToList();
+            return new Path(c);
+        }
+
+        public Path Mutate()
+        {
+            List<City.City> tmp = new List<City.City>(Cities);
+
+            if (Random.NextDouble() < Environment.Environment.MutateRate)
+            {
+                int i = Random.Next(0, Cities.Count);
+                int j = Random.Next(0, Cities.Count);
+                City.City v = tmp[i];
+                tmp[i] = tmp[j];
+                tmp[j] = v;
+            }
+
+            return new Path(tmp);
+        }
 
         private double CalculateDistance()
         {
